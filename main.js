a426aa53ba271d1c7a0b672b69d0c5ea20fbaa58
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-12 11:17:29 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+const Hand = {
+    Rock: "✊",
+    Paper: "✋",
+    Scissor: "✌️",
+};
+
+const win_case = [
+    ["Rock", "Scissor"],
+    ["Scissor", "Paper"],
+    ["Paper", "Rock"],
+];
+
+const showArea = document.getElementById("show");
+const result = document.getElementById("result");
+const img = document.getElementById("reimu-img");
+const panel = document.getElementById("panel");
+const modal = document.getElementById("modal");
+
+async function play(userHandId) {
+    const reimuHandId = Object.keys(Hand)[Math.floor(Math.random() * 3)];
+    await modalHand(reimuHandId);
+    await showHand(userHandId, reimuHandId);
+    await judge(userHandId, reimuHandId);
+}
+
+async function showHand(userHandId, reimuHandId) {
+    showArea.style.display = "block";
+    showArea.textContent = `${Hand[userHandId]} vs ${Hand[reimuHandId]}`;
+}
+
+async function judge(userHandId, reimuHandId) {
+    result.style.display = "block";
+    panel.style.pointerEvents = "none";
+
+    if (isWin([reimuHandId, userHandId])) {
+        result.textContent = "私の勝ち!";
+        img.src = "static/reimu_happy.jpg";
+    } else if (isWin([userHandId, reimuHandId])) {
+        result.textContent = "負けちゃった...";
+        img.src = "static/reimu_sad.jpg";
+    } else {
+        result.textContent = "あいこ";
+        img.src = "static/reimu_normal.jpg";
+    }
+
+    await sleep(2000);
+    result.textContent = "";
+    result.style.display = "none";
+    showArea.style.display = "none";
+    panel.style.pointerEvents = "initial";
+    img.src = "static/reimu_normal.jpg";
+}
+
+function isWin(target) {
+    return win_case.some(
+        (sub) =>
+            sub.length === target.length &&
+            sub.every((val, i) => val === target[i]),
+    );
+}
+
+async function modalHand(reimuHand) {
+    const openModal = () => {
+        modal.style.display = "block";
+        document.body.classList.add("modal-open");
+    };
+    const closeModal = () => {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open");
+    };
+    modal.textContent = Hand[reimuHand];
+
+    openModal();
+    await sleep(1000);
+    closeModal();
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
